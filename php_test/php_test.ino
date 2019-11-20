191110c7c626dbccb220205d5ab3ec8c318d9de4
Su-Kyung <slloph720@naver.com>
2019-11-20 21:41:24 +0900

    필요없는 부분 삭제(temperature)
    컴파일 시 에러나는 부분 주석처리(temperature)

--- php_test/php_test.ino
+++ php_test/php_test.ino
@@ -1,5 +1,5 @@
-#include <OneWire.h>    
-#include <DallasTemperature.h>    
+// #include <OneWire.h>    
+// #include <DallasTemperature.h>    
 #include <string.h>  
 
 #include "ESP8266.h"
@@ -26,13 +26,13 @@ SoftwareSerial mySerial(10, 9); /* RX:D10, TX:D9 */
 #define ONE_WIRE_BUS 3  
       
 //1-wire 디바이스와 통신하기 위한 준비    
-OneWire oneWire(ONE_WIRE_BUS);    
+//OneWire oneWire(ONE_WIRE_BUS);    
       
 // oneWire선언한 것을 sensors 선언시 참조함.    
-DallasTemperature sensors(&oneWire);    
+//DallasTemperature sensors(&oneWire);    
       
 //다비아스 주소를 저장할 배열 선언    
-DeviceAddress insideThermometer;    
+//DeviceAddress insideThermometer;    
       
       
       
@@ -126,118 +126,118 @@ void setup(void)
        
          
   //1-wire 버스 초기화    
-  sensors.begin();    
+//  sensors.begin();    
         
   //발견한 디바이스 갯수    
   Serial.print("Found ");    
-  Serial.print(sensors.getDeviceCount(), DEC);    
+//  Serial.print(sensors.getDeviceCount(), DEC);    
   Serial.println(" devices.");    
       
   // parasite power 모드일 때에는  2핀(GND와 DQ 핀)만 연결하면 됨.    
   Serial.print("Parasite power is: ");     
-  if (sensors.isParasitePowerMode()) Serial.println("ON");    
-  else Serial.println("OFF");    
+//  if (sensors.isParasitePowerMode()) Serial.println("ON");    
+//  else Serial.println("OFF");    
         
        
   //버스에서 첫번째 장치의 주소를 가져온다.    
-  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0");     
+//  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0");     
         
   //버스에서 발견한 첫번째 장치의 주소 출력    
   Serial.print("Device 0 Address: ");    
-  printAddress(insideThermometer);    
+//  printAddress(insideThermometer);    
   Serial.println();    
       
   //데이터시트에서 확인결과 9~12비트까지 설정 가능    
-  sensors.setResolution(insideThermometer, 10);    
+//  sensors.setResolution(insideThermometer, 10);    
        
   Serial.print("Device 0 Resolution: ");    
-  Serial.print(sensors.getResolution(insideThermometer), DEC);     
+//  Serial.print(sensors.getResolution(insideThermometer), DEC);     
   Serial.println();    
 }    
       
       
 // 온도를 출력하는 함수    
-void printTemperature(DeviceAddress deviceAddress)    
-{    
-  //섭씨 온도를 가져옴    
-  float  tempC = sensors.getTempC(deviceAddress);    
-        
-  Serial.print("Temp C: ");    
-  Serial.print(tempC);    
-  Serial.print(" Temp F: ");    
-        
-  //화씨 온도로 변환    
-  Serial.println(DallasTemperature::toFahrenheit(tempC));  
-  
-  
-  
-     String cmd = "AT+CIPSTART=\"TCP\",\"";  
-     cmd += SERVERIP;  
-     cmd += "\",80";  
-     Serial.println(cmd);  
-     mySerial.println(cmd);  
-     if(mySerial.find("Error"))  
-    {  
-      Serial.println( "TCP connect error" );  
-      return;  
-    }  
-     
-  
-    char test[20];  
-    String temp(floatToString(test,tempC, 2, 0));  
-      
-     cmd = "GET /Drone-Whac-A-Mole/insert_score1.php?num="+temp+"\r\n"; //insrt_score+뒤에 큐브번호.php
-     mySerial.print("AT+CIPSEND=");  
-     mySerial.println(cmd.length());  
-          
-       
-     Serial.println(cmd);  
-       
-       
-     if(mySerial.find(">"))  
-     {  
-       Serial.print(">");  
-       }else  
-       {  
-         mySerial.println("AT+CIPCLOSE");  
-         Serial.println("connect timeout");  
-         delay(1000);  
-         return;  
-       }  
-         
-       mySerial.print(cmd);  
-       delay(2000);  
-       //Serial.find("+IPD");  
-       while (Serial.available())  
-       {  
-         char c = Serial.read();  
-         mySerial.write(c);  
-         if(c=='\r') mySerial.print('\n');  
-       }  
-       Serial.println("====");  
-       delay(1000);  
-}    
+//void printTemperature(DeviceAddress deviceAddress)    
+//{    
+//  //섭씨 온도를 가져옴    
+//  float  tempC = sensors.getTempC(deviceAddress);    
+//        
+//  Serial.print("Temp C: ");    
+//  Serial.print(tempC);    
+//  Serial.print(" Temp F: ");    
+//        
+//  //화씨 온도로 변환    
+//  Serial.println(DallasTemperature::toFahrenheit(tempC));  
+//  
+//  
+//  
+//     String cmd = "AT+CIPSTART=\"TCP\",\"";  
+//     cmd += SERVERIP;  
+//     cmd += "\",80";  
+//     Serial.println(cmd);  
+//     mySerial.println(cmd);  
+//     if(mySerial.find("Error"))  
+//    {  
+//      Serial.println( "TCP connect error" );  
+//      return;  
+//    }  
+//     
+//  
+//    char test[20];  
+//    String temp(floatToString(test,tempC, 2, 0));  
+//      
+//     cmd = "GET /Drone-Whac-A-Mole/insert_score1.php?num="+temp+"\r\n"; //insrt_score+뒤에 큐브번호.php
+//     mySerial.print("AT+CIPSEND=");  
+//     mySerial.println(cmd.length());  
+//          
+//       
+//     Serial.println(cmd);  
+//       
+//       
+//     if(mySerial.find(">"))  
+//     {  
+//       Serial.print(">");  
+//       }else  
+//       {  
+//         mySerial.println("AT+CIPCLOSE");  
+//         Serial.println("connect timeout");  
+//         delay(1000);  
+//         return;  
+//       }  
+//         
+//       mySerial.print(cmd);  
+//       delay(2000);  
+//       //Serial.find("+IPD");  
+//       while (Serial.available())  
+//       {  
+//         char c = Serial.read();  
+//         mySerial.write(c);  
+//         if(c=='\r') mySerial.print('\n');  
+//       }  
+//       Serial.println("====");  
+//       delay(1000);  
+//}    
       
 //디바이스 주소를 출력하는 함수    
-void printAddress(DeviceAddress deviceAddress)    
-{    
-  for (uint8_t i = 0; i < 8; i++)    
-  {    
-    if (deviceAddress[i] < 16) Serial.print("0");    
-        Serial.print(deviceAddress[i], HEX);    
-  }    
-}    
+//void printAddress(DeviceAddress deviceAddress)    
+//{    
+//  for (uint8_t i = 0; i < 8; i++)    
+//  {    
+//    if (deviceAddress[i] < 16) Serial.print("0");    
+//        Serial.print(deviceAddress[i], HEX);    
+//  }    
+//}    
       
       
 void loop(void)    
 {     
   Serial.print("Requesting temperatures...");    
   //sensors.requestTemperatures();   //연결되어 있는 전체 센서의 온도 값을 읽어옴    
-  sensors.requestTemperaturesByIndex(0); //첫번째 센서의 온도값 읽어옴    
+//  sensors.requestTemperaturesByIndex(0); //첫번째 센서의 온도값 읽어옴    
   Serial.println("DONE");    
        
   //센서에서 읽어온 온도를 출력    
-  printTemperature(insideThermometer);    
+//  printTemperature(insideThermometer);    
 }    
   
   
